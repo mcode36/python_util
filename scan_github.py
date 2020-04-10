6c492a2fa02f90683750d39c508406bf758e54cd
mcode36 <48412323+mcode36@users.noreply.github.com>
2020-04-09 22:45:48 -0800

    initial commit

---
+++ scan_github.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+keywords = ['angular', 'flask', 'django', 'react', 'node.js', 'mysql', 'sqlite', 'mongodb']
+site = 'https://github.com/'
+
+result = {}
+max_term = 0
+max_num = 0
+for term in keywords:
+    url = site + 'search?q=' + term
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    for h in soup.find_all("h3"):
+        arr = h.text.split()
+        if 'repository' in arr:
+            num = arr[0].replace(',','')
+            if max_term < len(term): max_term = len(term)
+            if max_num < len(num): max_num  = len(num)
+            result[term] = int(num)
+            print(f'Retriving term \'{term}\' : {arr[0]}')
+
+## sort dictionary 'result'
+print()
+print('Sorted result:')
+tuples = sorted(result.items(), reverse=True, key = lambda kv:(kv[1], kv[0]))
+for t in tuples:
+    print(f'{t[0]:{max_term}} : {t[1]:{max_num}}')
